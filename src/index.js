652e415e66faa5cc8bff00ab8bf269f8fe84db7a
Dawid Szarwark <daq131@gmail.com>
2022-07-16 10:39:42 +0200

    getting current position

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const App = () => {
-    return <div>Hi ..</div>
+class App extends React.Component {
+    render () {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+                (err) => console.log(err)
+        );
+        return <div>Latitude: </div>
+    }
 }
 
-ReactDOM.render(
-    <App />, document.querySelector('#root') 
-    );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
