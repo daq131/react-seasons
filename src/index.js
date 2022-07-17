c911f8a2a1625049f0041072a9327ade89383242
Dawid Szarwark <daq131@gmail.com>
2022-07-17 20:59:14 +0200

    add lifecycle

--- src/index.js
+++ src/index.js
@@ -2,21 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    
+    state = {lat: null, errorMessage: '' };
 
-        this.state = {lat: null, errorMessage: '' };
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude });
-            },
-                (err) => {
-                    this.setState( {errorMessage: err.message} )
-                }
+            position => this.setState({ lat: position.coords.latitude }),
+                err => this.setState( {errorMessage: err.message} )
         );
-
     }
+
     render () {
         if(this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
@@ -27,6 +22,7 @@ class App extends React.Component {
         return <div>Loading !!!</div>
     }
 }
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
